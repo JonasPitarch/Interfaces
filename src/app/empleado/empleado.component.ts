351822180658a40f8f6c2b28a4a12b4842b19ada
jonas <jonaspitarch06@gmail.com>
2025-02-17 12:43:03 +0100

    Dia 14/02/2025

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  imports: [],
+  templateUrl: './empleado.component.html',
+  // template : "<p>Aqui iria un empleado</p>",
+  // styleUrl: './empleado.component.css'
+  styles : ["p{background-color: red}"]
+})
+export class EmpleadoComponent {
+
+}
